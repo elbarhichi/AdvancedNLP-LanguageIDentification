0afc2d7b8a0405aacc951b7e8f1dc0ebc2f0b2ab
elbarhichi <mohammed.elbarhichi@gmail.com>
2025-03-07 16:13:38 +0100

    added config

--- train_and_test_on_dce/mdeberta.sh
+++ train_and_test_on_dce/mdeberta.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -p gpu_prod_long            # Use the interactive GPU partition
-#SBATCH -t 19:00:00             
-#SBATCH -J lstone        # Name of the job
-#SBATCH -o outputs/output_mt5base_job_%j.out   # Output file (with job ID)
+#SBATCH -t 01:00:00             
+#SBATCH -J mdebertav3base        # Name of the job
+#SBATCH -o outputs/output_mdebertav3base_job_%j.out   # Output file (with job ID)
 
 echo "Job started on $(date)"
 echo "Running on host: $(hostname)"
@@ -12,11 +12,10 @@ nvidia-smi
 echo "Testing Python environment..."
 python --version
 
-
 echo "Training..."
-python training_scripts/mt5base/train_mt5base.py
+python training_scripts/mdebertav3/mdebertav3base.py
 
 echo "Testing..."
-python testing_scripts/mt5base/test_mt5base.py
+python testing_scripts/mdebertav3/testing_mdebertav3base.py
 
 echo "Job finished on $(date)"
